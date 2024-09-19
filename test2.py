a173d4e1dc132b7a84c19226825aaaa547560344
zumokuta1 <131922106+zumokuta1@users.noreply.github.com>
2024-09-19 15:48:39 -0500

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,17 @@
+#Parte 1
+a = 33
+b = 33
+
+if b > a:
+    print("B es mayor Que A")
+elif a > b:
+    print("A es mayor que b")
+else:
+    print("Son iguales")
+#Parte 2 
+def my_function(fname):
+    print(fname + "Refsnes")
+
+my_function("Emil ")
+my_function("Tobias ")
+my_function("Linux ")
